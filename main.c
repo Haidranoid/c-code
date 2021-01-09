970f762fad56a1d012203dd766f573338b0d219b
Jose Eduardo Martinez Lara <martinezlara_joseeduardo@hotmail.com>
2021-01-09 11:28:28 -0600

    compile options

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-
+    #ifdef MY_MACRO
+        printf("MY_MACRO is defined with value: %d\n",MY_MACRO);
+    #endif
+    printf("Hello, wold!\n");
     return 0;
 }
