b06efc27244779c6ec56aae60cf8b9c21f71cfe9
Administaff <administaff@MacBook-Pro-de-Administaff.local>
2021-04-21 17:16:41 -0500

    static library creation added

---
+++ libraries/my-lib/headers/mylib.h
@@ -0,0 +1,6 @@
+#ifndef MY_LIB_H
+#define MY_LIB_H
+
+void func(void);
+
+#endif // MY_LIB_H
---
+++ libraries/my-lib/sources/mylib.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "../headers/mylib.h"
+
+void func(void){
+    printf("func() called from static library");
+}
